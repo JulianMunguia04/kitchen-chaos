d73b5b850d1726c497fffed48a0f28db3f362e73
JulianMunguia04 <julianmunguia04@gmail.com>
2026-01-14 19:42:32 -0700

    Character Visual, Rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,16 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
+        float zPosition = 0f;
         if (Input.GetKey(KeyCode.W))
         {
             inputVector.y = +1;
@@ -26,8 +30,11 @@ public class Player : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
-        Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        Vector3 movDir = new Vector3(inputVector.x, zPosition, inputVector.y);
         transform.position += movDir * moveSpeed * Time.deltaTime;
+
+        //Rotating
+        transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotateSpeed);
         Debug.Log(inputVector);
     }
 }
