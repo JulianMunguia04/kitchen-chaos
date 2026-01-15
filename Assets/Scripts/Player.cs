dfb574e463c121641629ffcc449362f5189d7f21
JulianMunguia04 <julianmunguia04@gmail.com>
2026-01-14 18:56:56 -0700

    Initial Unity Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 movDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += movDir * moveSpeed * Time.deltaTime;
+        Debug.Log(inputVector);
+    }
+}
---
+++ Assets/Testing.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        if (transform == transform)
+        {
+            Update();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
